e5cfc090f8b7a2a0825568823808b5dc5682ab2e
nsedan <naim.alejandro.sedan@gmail.com>
2021-01-10 20:34:00 +0000

    Admin custom lists

--- workouts/admin.py
+++ workouts/admin.py
@@ -2,5 +2,32 @@ from django.contrib import admin
 from .models import Workout, Category
 
 # Register your models here.
-admin.site.register(Workout)
-admin.site.register(Category)
+
+
+class WorkoutAdmin(admin.ModelAdmin):
+    """
+    docstring
+    """
+    list_display = (
+        'name',
+        'category',
+        'difficulty',
+        'price',
+        'duration',
+        'rating'
+    )
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    docstring
+    """
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Workout, WorkoutAdmin)
+admin.site.register(Category, CategoryAdmin)

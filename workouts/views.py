1004ec1bf91cfba639eb0ad4a7966892ec2ca7ba
nsedan <naim.alejandro.sedan@gmail.com>
2021-01-29 12:18:38 +0000

    Added workouts view, template and url

--- workouts/views.py
+++ workouts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Workout
 
-# Create your views here.
+
+def all_workouts(request):
+    """ A view to return all workouts """
+
+    workouts = Workout.objects.all()
+
+    context = {
+        'workouts': workouts,
+    }
+
+    return render(request, 'workouts/workouts.html', context)

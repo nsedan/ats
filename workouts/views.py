ec59ee7efd1134aad42263ee06c94fcef3c2399d
nsedan <naim.alejandro.sedan@gmail.com>
2021-02-10 16:58:49 +0000

    Added workout sorting functionality

--- workouts/views.py
+++ workouts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Workout, Category
 
 
@@ -9,14 +11,34 @@ def all_workouts(request):
 
     workouts = Workout.objects.all()
     all_categories = Category.objects.all()
+    current_categories = None
     query = None
-    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                workouts = workouts.annotate(lower_name=Lower('name'))
+
+            # Allow to sort by category name instead of category ID
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            workouts = workouts.order_by(sortkey)
+
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            workouts = workouts.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            current_categories = request.GET['category'].split(',')
+            workouts = workouts.filter(category__name__in=current_categories)
+            current_categories = Category.objects.filter(
+                name__in=current_categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -29,11 +51,14 @@ def all_workouts(request):
                 description__icontains=query)
             workouts = workouts.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'workouts': workouts,
-        'all_categories': all_categories,
         'search_term': query,
-        'current_categories': categories,
+        'all_categories': all_categories,
+        'current_categories': current_categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'workouts/workouts.html', context)

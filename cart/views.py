cbe68b04506fba6e541859f062b9e80c68eee9fd
nsedan <naim.alejandro.sedan@gmail.com>
2021-02-16 11:58:14 +0000

    Added cart basic functionality

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a workout to the shopping cart """
+
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', [])
+
+    if item_id not in list(cart):
+        cart.append(item_id)
+    # else:
+        # TODO: disable add to cart botton on frontend
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+
+    return redirect(redirect_url)
